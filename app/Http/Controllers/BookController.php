c5b139ba3da75237958877d625e1d40a99948a98
Robbie Gay <robbiecgay@gmail.com>
2019-11-05 16:57:44 -0500

    Created DB, added fields to users, created books table

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // show a list of resources
+    public function index() {
+        return view('/books');
+    }
+
+    // show a specific resource (a user, article, list)
+    public function show() {
+        return view('/books/{id}');
+    }
+
+    // create a resource
+    public function create() {
+        return view('books/create');
+    }
+
+    // store (or persist) that resource
+    public function store() {
+        // Article::create(request()->validate([
+        //     'title' => request('title'),
+        //     'excerpt' => request('excerpt'),
+        //     'body' => request('body')
+        // ]));
+
+        return redirect('/books');
+    }
+
+    // edit an existing resource
+    public function edit() {
+        return view('/books/{id}/edit');
+    }
+
+    // persist that edit
+    public function update() { //Article $article
+        // $article->title = request('title');
+        // $article->excerpt = request('excerpt');
+        // $article->body = request('body');
+        // $article->save();
+
+        return redirect('/books' . $book->id);
+    }
+
+    // delete a resource
+    public function destroy() {
+        
+        return redirect('/books');
+    }
+}
--- database/migrations/2019_11_05_200454_create_books_table.php
+++ database/migrations/2019_11_05_200454_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('isbn', 13);
             $table->string('name');
-            $table->string('user_name')->unique();
-            $table->string('password');
-            $table->string('email')->unique();
-            $table->string('phone_num')->nullable(); 
-            $table->text('address')->nullable();
-            // $table->bigIncrements('card_num');
-            $table->rememberToken();
+            $table->string('author');
+            // array for keywords
+            $table->string('keywords');            
+            $table->text('blurb')->nullable();
+            $table->date('release_date');
+            $table->string('media_type')->default('book');
             $table->timestamps();
         });
     }
@@ -34,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 }
